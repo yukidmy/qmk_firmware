559ab66541a4e68bd229c2055b2a7ac846608dfb
Yuki Y <yukidmy@gmail.com>
2025-02-05 23:21:45 +0900

    Update keymap to match with Moonlander layout

--- keyboards/ergodash/rev1/keymaps/yuki/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yuki/keymap.c
@@ -18,87 +18,47 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  =   |   1  |   2  |   3  |   4  |   5  |      |                    |Pscree|   6  |   7  |   8  |   9  |   0  |  -   |
+   * |  =   |   1  |   2  |   3  |   4  |   5  |Pscree|                    | Del  |   6  |   7  |   8  |   9  |   0  |  -   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  `   |   Q  |   W  |   E  |   R  |   T  |      |                    |      |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |  `   |   Q  |   W  |   E  |   R  |   T  |      |                    | PgUp |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  | PgNn |                    | PgUp |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |                    | PgDn |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  ||||||||      ||||||||  ESC ||||||||   N  |   M  |   ,  |   .  |   /  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  ||||||||  ESC ||||||||  Alt ||||||||   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Lower| GUI  | Alt  |      |||||||| Tab  | Space| Lower|||||||| Alt  | Enter| Bksp |||||||| Left | Down |  Up  | Right|
+   * | Ctrl | Alt  |  GUI |      |||||||| Tab  | Space| Lower||||||||  GUI | Enter| Bksp |||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    KC_EQL,  KC_1,    KC_2,    KC_3,       KC_4,    KC_5,  _______,                      KC_PSCR, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-    KC_GRV,  KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,  _______,                      _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,       KC_F,    KC_G,  KC_PGDN,                      KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,    KC_B,           _______,   KC_ESC,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    LOWER,   KC_LGUI, KC_LALT, _______,           KC_TAB,  KC_SPC,  LOWER,     KC_LALT,  KC_ENT,  KC_BSPC,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
+    KC_EQL,  KC_1,    KC_2,    KC_3,       KC_4,    KC_5,  KC_PSCR,                      KC_DEL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+    KC_GRV,  KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,  _______,                      KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,       KC_F,    KC_G,  _______,                      KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,    KC_B,           KC_ESC,    KC_LALT,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LCTL, KC_LALT, KC_LGUI, _______,           KC_TAB,  KC_SPC,  LOWER,     KC_RGUI,  KC_ENT,  KC_BSPC,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
   ),
 
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |      |                    |Pscree|  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
+   * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |Pscree|                    | Del  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  `   |   !  |   @  |   #  |   $  |   %  |      |                    |      |   ^  |   &  |   *  |   (  |   )  |  F12 |
+   * |  `   |   !  |   @  |   #  |   $  |   %  |      |                    | PgUp |   ^  |   &  |   *  |   (  |   )  |  F12 |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   1  |   2  |   3  |   4  |   5  | PgDn |                    | PgUp |   H  | Left | Down |  Up  | Right|  "   |
+   * | Ctrl |   1  |   2  |   3  |   4  |   5  |      |                    | PgDn |   H  | Left | Down |  Up  | Right|  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   6  |   7  |   8  |   9  |   0  |      |PageDn|      |PageUp|      |   N  |   [  |   ]  |   {  |   }  | Shift|
+   * | Shift|   6  |   7  |   8  |   9  |   0  |      |  ESC |      |  Alt |      |   N  |   [  |   ]  |   {  |   }  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Lower| GUI  | Alt  |      |||||||| Tab  | Space| Lower|||||||| Alt  | Enter| Bksp |||||||| Left | Down |  Up  | Right|
+   * | Ctrl | Alt  |  GUI |      |||||||| Tab  | Space| Lower||||||||  GUI | Enter| Bksp |||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                     KC_PSCR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                     _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12,  \
-    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PGDN,                     KC_PGUP, KC_H,    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_DQT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,             _______,  KC_ESC,            KC_N,    KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_RSFT, \
-    LOWER,   KC_LGUI, KC_LALT, _______,          KC_TAB,  KC_SPC,  LOWER,    KC_LALT,  KC_ENT,  KC_BSPC,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_PSCR,                       KC_DEL,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                       KC_PGUP, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12,  \
+    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,                       KC_PGDN, KC_H,    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_DQT,  \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_ESC,    KC_LALT,           KC_N,    KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_RSFT, \
+    KC_LCTL, KC_LALT, KC_LGUI, _______,          KC_TAB,  KC_SPC,   LOWER,     KC_RGUI,  KC_ENT,  KC_BSPC,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
   ),
 
-  /* Raise (= Lower)
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |      |                    |Pscree|  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
-   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  `   |   !  |   @  |   #  |   $  |   %  |      |                    |      |   ^  |   &  |   *  |   (  |   )  |  F12 |
-   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   1  |   2  |   3  |   4  |   5  | PgDn |                    | PgUp |   H  | Left | Down |  Up  | Right|  "   |
-   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   6  |   7  |   8  |   9  |   0  |      |PageDn|      |PageUp|      |   N  |   [  |   ]  |   {  |   }  | Shift|
-   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Lower| GUI  | Alt  |      |||||||| Tab  | Space| Lower|||||||| Alt  | Enter| Bksp |||||||| Left | Down |  Up  | Right|
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   */
-  [_LOWER] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                     KC_PSCR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                     _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12,  \
-    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PGDN,                     KC_PGUP, KC_H,    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_DQT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,             _______,  KC_ESC,            KC_N,    KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_RSFT, \
-    LOWER,   KC_LGUI, KC_LALT, _______,          KC_TAB,  KC_SPC,  LOWER,    KC_LALT,  KC_ENT,  KC_BSPC,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
-  ),
-
-  /* Adjust
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
-   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      | Reset|RGB ON|  MODE|  HUE-|  HUE+|      |                    |      |  SAT-|  SAT+|  VAL-|  VAL+|      |      |
-   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      | BL ON|  BRTG|  INC |  DEC |      |                    |      |      |      |      |      |      |      |
-   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
-   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * |      |      |      |      ||||||||      |      |      ||||||||      |      |      ||||||||      |      |      |      |
-   * ,----------------------------------------------------------------------------------------------------------------------.
-   */
-  [_ADJUST] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,_______,                        _______, _______, _______, _______, _______, _______, _______, \
-    _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,_______,                        _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
-    _______, _______, BL_TOGG, BL_BRTG, BL_INC , BL_DEC ,_______,                        _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______,_______,                        _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______,          _______,_______,_______,        _______,_______, _______,          _______, _______, _______, _______  \
-  )
+  // RAISE & ADJUST - not used
 };
 
 #ifdef AUDIO_ENABLE
--- keyboards/ergodash/rev1/keymaps/yuki/write.sh
+++ keyboards/ergodash/rev1/keymaps/yuki/write.sh
@@ -1,3 +1,8 @@
-cd ~/qmk_firmware
+#!/bin/sh
+set -e
+
+cd ~/dev/qmk_firmware
+
+# See https://docs.qmk.fm/newbs_getting_started for env setup
 make ergodash/rev1:yuki
 make ergodash/rev1:yuki:avrdude
